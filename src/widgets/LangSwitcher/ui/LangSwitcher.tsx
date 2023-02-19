ac0bea71802dbd6b9caafe0a1b1da518b8b1b2e4
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2023-02-19 13:51:25 +0400

    Add stylelint, fix errors

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,6 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import classes from './LangSwitcher.module.scss';
 import { useTranslation } from "react-i18next";
 
 
@@ -18,7 +17,7 @@ export const LangSwitcher = ({className}: LangSwitcherProps) => {
   return (
       <Button
         theme={ThemeButton.CLEAR}
-        className={classNames(classes.ThemeSwitcher, [className])}
+        className={classNames('', [className])}
         onClick={toggle}
       >
         {t('language')}
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,7 +1,6 @@
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import classes from './ThemeSwitcher.module.scss';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 
@@ -16,7 +15,7 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
   return (
     <Button
       onClick={ toggleTheme }
-      className={classNames(classes.ThemeSwitcher, [className])}
+      className={classNames('', [className])}
       theme={ThemeButton.CLEAR}
     >
       {theme === Theme.DARK ? <DarkIcon/> : <LightIcon />}
