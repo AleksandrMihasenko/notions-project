34a001ad36bde9e8d75c07b28eab0314556871c9
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2023-04-23 15:07:48 +0200

    Add custom text component

---
+++ src/shared/ui/Text/Text.stories.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { type ComponentMeta, type ComponentStory } from '@storybook/react';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+
+export default {
+  title: 'share/Text',
+  component: Text,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Text>;
+
+const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  title: 'Text title',
+  text: 'Lorem impsum',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  title: 'Text title',
+  text: 'Lorem impsum',
+  theme: TextTheme.ERROR,
+};
+
+export const OnlyTitle = Template.bind({});
+OnlyTitle.args = {
+  title: 'Text title',
+};
+
+export const OnlyText = Template.bind({});
+OnlyText.args = {
+  text: 'Lorem impsum',
+};
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+  title: 'Text title',
+  text: 'Lorem impsum',
+};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OnlyTitleDark = Template.bind({});
+OnlyTitleDark.args = {
+  title: 'Text title',
+};
+OnlyTitleDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OnlyTextDark = Template.bind({});
+OnlyTextDark.args = {
+  text: 'Lorem impsum',
+};
+OnlyTextDark.decorators = [ThemeDecorator(Theme.DARK)];
---
+++ src/shared/ui/Text/Text.tsx
@@ -0,0 +1,36 @@
+import { type FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Theme } from 'app/providers/ThemeProvider';
+import classes from './Text.module.scss';
+
+export enum TextTheme {
+  PRIMARY = 'primary',
+  ERROR = 'error',
+}
+
+interface TextProps {
+  className?: string;
+  title?: string;
+  text?: string;
+  theme?: TextTheme;
+}
+
+export const Text: FC<TextProps> = (props) => {
+  const {
+    className,
+    title,
+    text,
+    theme = TextTheme.PRIMARY,
+  } = props;
+
+  const mods: Record<string, boolean | string | undefined> = {
+    [classes[theme ?? '']]: true,
+  };
+
+  return (
+    <div className={classNames(classes.Button, [className as string], mods)}>
+      {title && <p className={classes.title}>{title}</p>}
+      {text && <p className={classes.text}>{text}</p>}
+    </div>
+  );
+};
