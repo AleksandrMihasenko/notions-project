be9ab6f7d3a0f9f8a3dfe06c2eef6aeec1082ad7
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2023-05-16 18:34:09 +0200

    Fix json server and loki tests

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,15 +10,6 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env'],
-        plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['en', 'ru'],
-              keyAsDefaultValue: true,
-            },
-          ],
-        ],
       },
     },
   };
