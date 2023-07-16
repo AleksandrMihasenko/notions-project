87dab3c310ef534110e89e9135b51309c828c14c
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2023-07-16 10:38:55 +0200

    Test

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,1 @@
-import { createRoot } from 'react-dom/client';
-import { StoreProvider } from 'app/providers/StoreProvider';
-import { BrowserRouter } from 'react-router-dom';
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
-import ThemeProvider from 'app/providers/ThemeProvider/ui/ThemeProvider';
-import App from './app/App';
-
-import 'shared/config/i18n/i18n';
-
-import 'app/styles/index.scss';
-
-const container = document.getElementById('root');
-const root = createRoot(container!);
-
-
-root.render(
-  <BrowserRouter>
-    <StoreProvider>
-      <ErrorBoundary>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </ErrorBoundary>
-    </StoreProvider>
-  </BrowserRouter>,
-);
+import { createRoot } from 'react-dom/client';import { StoreProvider } from 'app/providers/StoreProvider';import { BrowserRouter } from 'react-router-dom';import { ErrorBoundary } from 'app/providers/ErrorBoundary';import ThemeProvider from 'app/providers/ThemeProvider/ui/ThemeProvider';import App from './app/App';import 'shared/config/i18n/i18n';import 'app/styles/index.scss';const container = document.getElementById('root');const root = createRoot(container!);root.render(  <BrowserRouter>    <StoreProvider>      <ErrorBoundary>        <ThemeProvider>          <App />        </ThemeProvider>      </ErrorBoundary>    </StoreProvider>  </BrowserRouter>,);
